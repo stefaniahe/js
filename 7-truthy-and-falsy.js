f2b6e477bb017448c4b93e6053572f721efafe05
stefania herrera <herrerastefania904@gmail.com>
2020-10-05 20:48:44 -0500

    Create 7-truthy-and-falsy.js

---
+++ 7-truthy-and-falsy.js
@@ -0,0 +1,14 @@
+//Son por defecto falsos
+console.log(Boolean(0));
+console.log(Boolean(null));
+console.log(Boolean(NaN)); //Not a number
+console.log(Boolean(undefined));
+console.log(Boolean(false));
+console.log(Boolean(""));
+
+//Los verdaderos
+console.log(Boolean(-50));
+console.log(Boolean("dfasdf"));
+console.log(Boolean([]));
+console.log(Boolean({}));
+console.log(Boolean(function(){}));
