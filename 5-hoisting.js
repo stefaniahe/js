0b6c625beafcbfc9fd23fcbd064986c47a24fc69
stefania herrera <herrerastefania904@gmail.com>
2020-10-05 20:48:22 -0500

    Create 5-hoisting.js

---
+++ 5-hoisting.js
@@ -0,0 +1,28 @@
+var miNombre;
+miNombre = "Javier";
+
+var miNombre = "Javier";
+
+//
+console.log(miNombre);
+var miNombre = "Javier";
+
+//Otro caso
+miApellido = "Collazos";
+console.log(miApellido);
+var miApellido; // -> Hoisting
+
+//
+hola();
+
+function hola(){
+    console.log("Hola");
+}
+
+
+//
+saludar();
+
+var saludar = function(){
+    console.log("Saludando");
+}
