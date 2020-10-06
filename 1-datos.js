ab23012df713234aef7791ef62e70a6fb0a0863c
stefania herrera <herrerastefania904@gmail.com>
2020-10-05 20:47:14 -0500

    datos

---
+++ 1-datos.js
@@ -0,0 +1,23 @@
+//Valores primitivos
+40 
+
+"Javier"
+'Javier'
+
+//boolean
+true
+false
+
+//----
+null
+undefined
+//----
+
+//tipos objeto - no primitivos
+//arreglos
+[1,2,3];
+
+//objetos
+{
+    nombre: "Javier"
+}
