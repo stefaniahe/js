8e637afc389234dc51459a90a62c9468507ffc02
stefania herrera <herrerastefania904@gmail.com>
2020-10-05 20:48:56 -0500

    Create 8-operadores.js

---
+++ 8-operadores.js
@@ -0,0 +1,39 @@
+//Aritméticos
+
+5 + 2
+2 - 7
+5 / ( 3 + 2 )
+9 * 5 
+10 % 2
+2 ** 5 == 2*2*2*2*2
+
+
+"Javier" + "Collazos" //-> Concatenar
+
+!true //La negación //Invierte cualquier tipo boolean
+
+//operadores de asignación
+var a = 1;
+
+//Comparación //SIEMPRE NOS DA COMO RESPUESTA UN BOOLEAN 
+//number    //String
+3       ==   "3"; //true
+
+//number        //String
+3       ===     "3"; //false
+
+5 < 5; //false
+5 > 3; //true
+5 <= 5; //true
+5 >= 9; //false
+
+//true      true
+(a     &&   b) === true //AND
+
+//false     true
+(a     ||   b) === true //OR
+
+var edad = 20;
+edad++; //incrementar en 1
+edad += 2; // edad = edad + 2;
+edad *= 50; // edad = edad * 50;
